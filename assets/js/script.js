9443be689259811dad17103cc168fc91fe99100c
katiullahaxioned <atiullahk@axioned>
2022-08-25 12:45:46 +0530

    functionality added for top control buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-// ********************************************************************* //
-// ***************** 1st sliding effect ******************************** //
-// ********************************************************************* //
-
 var prev = document.querySelector(".prev");
 var next = document.querySelector(".next");
 var list = document.querySelector(".list");
@@ -14,75 +10,46 @@ var listLength = listItems.length;
 
 function slideList(positionLeft) {
   list.style.left = positionLeft + "%";
+
+  if (positionLeft === -(percent * (listLength - 1))) {
+    next.classList.add("active");
+  } else {
+    next.classList.remove("active");
+  }
+
+  if (positionLeft === 0) {
+    prev.classList.add("active");
+  } else {
+    prev.classList.remove("active");
+  }
 }
 slideList(positionLeft);
 
+// top control functionality
+
 next.addEventListener("click", function () {
-	positionLeft = positionLeft - percent;
-  if (positionLeft === -(percent * listLength)) {
-		positionLeft = 0;
-  }
-	slideList(positionLeft);
-	console.log(positionLeft);
+  positionLeft = positionLeft - percent;
+  slideList(positionLeft);
 });
 
 prev.addEventListener("click", function () {
   positionLeft = positionLeft + percent;
-  if (positionLeft > 0) {
-    positionLeft = -(percent * (listLength - 1));
-  }
   slideList(positionLeft);
-	console.log(positionLeft);
 });
 
 // bottom control functionality
+
 var a = 0;
-function slideList2(a) {
-	list.style.left = -a + "%";
+
+function slideListBottom(a) {
+  list.style.left = -a + "%";
 }
-slideList2(a);
-bottomControl.forEach(function(Control, index) {
-	Control.addEventListener('click', function() {
-		a = percent * index;
-		slideList2(a);
-		console.log(a);
-	})
-})
 
-// ********************************************************************* //
-// ***************** 2nd sliding effect ******************************** //
-// ********************************************************************* //
+slideListBottom(a);
 
-// var prev = document.querySelector(".prev");
-// var next = document.querySelector(".next");
-// var listItems = document.querySelectorAll(".list-items");
-// var bottomControl = document.querySelectorAll(".bottom-control");
-
-// var initialnum = 0;
-
-// function slideList(num) {
-//   for (var item of listItems) {
-//     item.classList.add("display-none");
-//   }
-//   listItems[num].classList.remove("display-none");
-// }
-
-// slideList(initialnum);
-
-// prev.addEventListener("click", function () {
-//   var x = -1;
-//   initialnum += x;
-//   if (initialnum < 0) {
-//     initialnum = (listItems.length - 1);
-//   }
-// 	slideList(initialnum);
-// });
-
-// next.addEventListener("click", function () {
-// 	var y = 1;
-//   initialnum += y;
-//   if (initialnum === listItems.length) {
-// 		initialnum = 0;
-//   }
-//   slideList(initialnum);
-// });
+bottomControl.forEach(function (Control, index) {
+  Control.addEventListener("click", function () {
+    a = percent * index;
+    slideListBottom(a);
+  });
+});
